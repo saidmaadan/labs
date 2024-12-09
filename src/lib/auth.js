6253551186066e0fbd492f908a192125ec83940a
Said Maadan <saidmaadan@gmail.com>
2024-12-09 12:27:42 -0600

    Updated auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -23,6 +23,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  trustHost: true,
+  cookies: {
+    sessionToken: {
+      name: `${process.env.NODE_ENV === "production" ? "__Secure-" : ""}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === "production",
+        domain: process.env.NODE_ENV === "production" ? ".inventivelabs.co" : undefined
+      }
+    }
+  },
   callbacks: {
     async session({ token, session }) {
       if (token) {
