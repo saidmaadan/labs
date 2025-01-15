e5dbf2a1af4de0d31a1cabdf907948e48a599005
Said Maadan <saidmaadan@gmail.com>
2025-01-15 01:34:05 -0600

    updated terms

--- src/app/(main)/terms/page.jsx
+++ src/app/(main)/terms/page.jsx
@@ -1,44 +1,45 @@
 export default function Terms() {
     return (
-        <div className="py-20">
+        <div className="container-center py-20">
             
-            <div className="container-center">
-                <h1 className="text-2xl mb-6 font-bold">Terms of Service</h1>
-                <p className="text-lg mb-6">Thank you for choosing InventiveLabs for your web development needs. By using our services, you agree to the following terms of service:</p>
+              <div className="mb-10">
+                  <h1 className="text-2xl mb-6 font-bold">Terms of Service</h1>
+                  <p className="text-lg mb-6">Thank you for choosing InventiveLabs for your web development needs. By using our services, you agree to the following terms of service:</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Acceptance of Terms</h2>
-                <p className="text-sm mb-6">By using the services provided by InventiveLabs, a web development company based in Austin, Texas ("the Company"), you ("the Client") agree to be bound by the following Terms of Service and Conditions ("the Agreement"). The Company reserves the right to update or modify the Agreement at any time without prior notice.</p>
+                  <h2 className="text-xl mb-4">Acceptance of Terms</h2>
+                  <p className="text-sm mb-6">By using the services provided by InventiveLabs, a web development company based in Austin, Texas ("the Company"), you ("the Client") agree to be bound by the following Terms of Service and Conditions ("the Agreement"). The Company reserves the right to update or modify the Agreement at any time without prior notice.</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Scope of Services</h2>
-                <p className="text-sm mb-6">InventiveLabs provides web/mobile development, and related services as specified in the project proposal or Statement of Work agreed upon by both parties. We work with clients to develop websites, web applications, and other digital products. We will deliver the services in a professional and timely manner, using reasonable skill and care.</p>
+                  <h2 className="text-xl mb-4">Scope of Services</h2>
+                  <p className="text-sm mb-6">InventiveLabs provides web/mobile development, and related services as specified in the project proposal or Statement of Work agreed upon by both parties. We work with clients to develop websites, web applications, and other digital products. We will deliver the services in a professional and timely manner, using reasonable skill and care.</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Intellectual Property</h2>
-                <p className="text-sm mb-6">Upon receipt of full payment, the Company shall transfer all rights, titles, and interests in the final deliverables, including copyright and other intellectual property rights, to the Client. The Company retains the right to use the completed project and any preliminary designs for promotional purposes, in its portfolio, or as samples for future clients.</p>
+                  <h2 className="text-xl mb-4">Intellectual Property</h2>
+                  <p className="text-sm mb-6">Upon receipt of full payment, the Company shall transfer all rights, titles, and interests in the final deliverables, including copyright and other intellectual property rights, to the Client. The Company retains the right to use the completed project and any preliminary designs for promotional purposes, in its portfolio, or as samples for future clients.</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Confidentiality</h2>
-                <p className="text-sm mb-6">Both parties agree to maintain the confidentiality of any proprietary or confidential information received from the other party during the course of the project. This includes, but is not limited to, trade secrets, client data, and business processes. Confidential information shall not be disclosed to any third party without the prior written consent of the disclosing party.</p>
+                  <h2 className="text-xl mb-4">Confidentiality</h2>
+                  <p className="text-sm mb-6">Both parties agree to maintain the confidentiality of any proprietary or confidential information received from the other party during the course of the project. This includes, but is not limited to, trade secrets, client data, and business processes. Confidential information shall not be disclosed to any third party without the prior written consent of the disclosing party.</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Termination</h2>
-                <p className="text-sm mb-6">Either party may terminate the Agreement with written notice if the other party breaches any material term or condition of the Agreement, and such breach remains uncured for a period of thirty (30) days following receipt of written notice. Upon termination, the Client shall be responsible for any outstanding fees for work completed up to the date of termination.</p>
+                  <h2 className="text-xl mb-4">Termination</h2>
+                  <p className="text-sm mb-6">Either party may terminate the Agreement with written notice if the other party breaches any material term or condition of the Agreement, and such breach remains uncured for a period of thirty (30) days following receipt of written notice. Upon termination, the Client shall be responsible for any outstanding fees for work completed up to the date of termination.</p>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Warranties and Limitation of Liability</h2>
-                <div className="mb-10">
-                <p className="text-sm mb-6">The Company warrants that the services provided will be performed in a professional and workmanlike manner, in accordance with generally accepted industry standards. The Client's sole remedy for any breach of this warranty shall be the correction of any defects or deficiencies in the services.</p>
-                <h2 className="text-xl mb-4">EXCEPT AS EXPRESSLY PROVIDED IN THIS AGREEMENT, THE COMPANY DISCLAIMS ALL OTHER WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NONINFRINGEMENT. IN NO EVENT SHALL THE COMPANY BE LIABLE FOR ANY INDIRECT, INCIDENTAL, CONSEQUENTIAL, SPECIAL, OR EXEMPLARY DAMAGES ARISING OUT OF OR IN CONNECTION WITH THIS AGREEMENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</h2>
+                  <h2 className="text-xl mb-4">Warranties and Limitation of Liability</h2>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Indemnification</h2>
-                <p className="text-sm mb-6">Each party shall indemnify and hold harmless the other party, its employees, agents, and representatives from any and all claims, damages, losses, liabilities, and expenses (including reasonable attorney's fees) arising out of or in connection with any breach of this Agreement, except to the extent such claims, damages, losses, liabilities, or expenses are attributable to the negligence or willful misconduct of the indemnified party.</p>
+                  <p className="text-sm mb-6">The Company warrants that the services provided will be performed in a professional and workmanlike manner, in accordance with generally accepted industry standards. The Client's sole remedy for any breach of this warranty shall be the correction of any defects or deficiencies in the services.</p>
+                  <h2 className="text-xl mb-4">EXCEPT AS EXPRESSLY PROVIDED IN THIS AGREEMENT, THE COMPANY DISCLAIMS ALL OTHER WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NONINFRINGEMENT. IN NO EVENT SHALL THE COMPANY BE LIABLE FOR ANY INDIRECT, INCIDENTAL, CONSEQUENTIAL, SPECIAL, OR EXEMPLARY DAMAGES ARISING OUT OF OR IN CONNECTION WITH THIS AGREEMENT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.</h2>
                 </div>
                 <div className="mb-10">
-                <h2 className="text-xl mb-4">Governing Law and Dispute Resolution</h2>
-                <p className="text-sm mb-6">This Agreement shall be governed by and construed in accordance with the laws of the State of Texas, without regard to its conflicts of law principles. Any disputes arising out of or in connection with this Agreement shall be resolved through good faith negotiations, and if such negotiations fail, the parties agree to submit the dispute to binding arbitration in Austin, Texas,</p>
+                  <h2 className="text-xl mb-4">Indemnification</h2>
+                  <p className="text-sm mb-6">Each party shall indemnify and hold harmless the other party, its employees, agents, and representatives from any and all claims, damages, losses, liabilities, and expenses (including reasonable attorney's fees) arising out of or in connection with any breach of this Agreement, except to the extent such claims, damages, losses, liabilities, or expenses are attributable to the negligence or willful misconduct of the indemnified party.</p>
+                </div>
+                <div className="mb-10">
+                  <h2 className="text-xl mb-4">Governing Law and Dispute Resolution</h2>
+                  <p className="text-sm mb-6">This Agreement shall be governed by and construed in accordance with the laws of the State of Texas, without regard to its conflicts of law principles. Any disputes arising out of or in connection with this Agreement shall be resolved through good faith negotiations, and if such negotiations fail, the parties agree to submit the dispute to binding arbitration in Austin, Texas,</p>
                 </div>
             </div>
         </div>
